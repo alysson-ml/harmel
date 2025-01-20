db83560cd6e8130c23311b8eb88eeac340f771b3
Alysson Mello de Lara <alyssonlara99@gmail.com>
2025-01-20 15:10:35 -0300

    fix: corrige o caminho para o arquivo css da página

--- src/php/login.php
+++ src/php/login.php
@@ -46,7 +46,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Harmel - Login</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="../css/login.css">
     <!-- Bootstrap CSS --> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome --> 
